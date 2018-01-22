d2134c25130d040e0880c27bd74dcf2ac7bd499b
Fuerback <felipefuerback@hotmail.com>
2018-01-22 09:25:53 -0200

    botão enable/disable e mensagem de informação

--- WebContent/controller/dadosCriaPedido.js
+++ WebContent/controller/dadosCriaPedido.js
@@ -40,59 +40,73 @@ criaPedidoModulo.controller('CriaPedidoController', function ($scope, $http) {
   }
 
   $scope.salvarPedido = function(){
-	$http.post(urlPedido, $scope.pedido).then(function(pedido) {
-	  $scope.pedidos.push(pedido.data.data);
-	  $scope.listarPedidos();
-	  $scope.limparCampos();
-	}).catch(function(erro) {
-	  alert(erro);
-	});  
+	  $http.post(urlPedido, $scope.pedido).then(function(pedido) {
+		  $scope.pedidos.push(pedido.data.data);
+		  $scope.listarPedidos();
+		  $scope.limparCampos();
+	  }).catch(function(erro) {
+		  alert(erro);
+	  });
   }
 
   $scope.limparCampos = function(){
-    $scope.pedido = null;
+	  $scope.pedido = null;
   }
 
   $scope.selecionaPedido = function(pedidoSelecionado){
-    $scope.pedido = pedidoSelecionado;
+	  $scope.pedido = pedidoSelecionado;
   }
   
   $scope.atualizaCampos = function() {
-	if($scope.pedido.item != undefined) {
-      $scope.atualizaPreco();
-      $scope.atualizaQuantidade();
-      $scope.atualizaRentabilidade();
-    }
+	  if($scope.pedido.item != undefined) {
+		  $scope.atualizaPreco();
+		  $scope.atualizaQuantidade();
+		  $scope.atualizaRentabilidade();
+	  }
   }
   
   $scope.atualizaPreco = function(){
 	  $scope.pedido.preco = $scope.pedido.item.preco;
-	  }
+  }
   
   $scope.atualizaQuantidade = function(){
 	  document.getElementsByClassName("quantidadeInput")[0].setAttribute("step", $scope.pedido.item.multiplo);
       document.getElementsByClassName("quantidadeInput")[0].setAttribute("value", $scope.pedido.item.multiplo);
-	  }
+      document.getElementsByClassName("quantidadeInput")[0].setAttribute("min", $scope.pedido.item.multiplo);
+  }
   
   $scope.atualizaRentabilidade = function() {
 	  if($scope.pedido.item != undefined) {
 		  if($scope.pedido.item.preco < $scope.pedido.preco) {
 			  document.getElementsByClassName("rentabilidadeInput")[0].setAttribute("value", "Ótima");
+			  $scope.atualizaBotaoComAlerta(false);
 		  } else {
 			  var porcentagem = 0.1;
 			  var valorPorcentagem = $scope.pedido.item.preco*porcentagem;
 			  var valorMaximoDesconto = $scope.pedido.item.preco-valorPorcentagem;			  
 			  if ($scope.pedido.preco < valorMaximoDesconto) {
 				  document.getElementsByClassName("rentabilidadeInput")[0].setAttribute("value", "Ruim");
+				  $scope.atualizaBotaoComAlerta(true);
 			  } else {
 				  document.getElementsByClassName("rentabilidadeInput")[0].setAttribute("value", "Boa");
+				  $scope.atualizaBotaoComAlerta(false);
 			  }
 		  }
+	  }
+  }
+  
+  $scope.atualizaBotaoComAlerta = function(disabled) {
+	  document.getElementById("botao").disabled = false;
+	  document.getElementById("alerta").style.display = "none";
+	  if(disabled) {
+		  document.getElementById("botao").disabled = true;
+		  document.getElementById("alerta").style.display = "block";
 	  }
   }
   
   $scope.listarClientes();
   $scope.listarItens();
   $scope.listarPedidos();
+  document.getElementById("alerta").style.display = "none";
 
 })
